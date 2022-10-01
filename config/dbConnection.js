229eeba6952777eadaf6969b6e957309d965a7b8
Dimas Khaerudin <dimaskhaerudin666@gmail.com>
2022-10-01 10:27:22 +0700

    deploy

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
 
 const db = mysql.createPool({
-  host: "localhost", // "localhost" by default
-  user: "root", // "root" by default
-  password: "password",
-  database: "cookpedia",
+  host: "us-cdbr-east-06.cleardb.net", // "localhost" by default
+  user: "b584594a62040a", // "root" by default
+  password: "9bfd847c",
+  database: "heroku_a71458f9e4fdc29",
 });
 
 module.exports = db;
